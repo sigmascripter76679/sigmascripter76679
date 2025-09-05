e3a2a122714247f5882f032ac7db90ac18054c35
sigmascripter76679 <yibnjalal175@gmail.com>
2025-09-05 18:48:29 -0400

    liquid.js

---
+++ liquid.js
@@ -0,0 +1,10 @@
+elements.liquid_element = {
+    color: "#6e28d9",
+    behavior: behaviors.LIQUID,
+    category: "test",
+    state: "liquid",
+    stateHigh: "carbon_dioxide",
+    tempHigh: 100000,
+    tempLow:0,
+    stateLow: "ice",
+}
